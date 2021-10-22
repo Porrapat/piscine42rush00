a6fc38ababc3827e1abff586db3df00c4a5d1d2b
Sirada Lertpimonchai <slertpim@z1t4c1.42bangkok.com>
2021-10-23 01:02:33 +0700

    fix

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 /*   By: slertpim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 22:34:33 by slertpim          #+#    #+#             */
-/*   Updated: 2021/10/22 22:37:12 by slertpim         ###   ########.fr       */
+/*   Updated: 2021/10/23 01:01:31 by slertpim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 void	rush(int i, int j);
 
-int	main()
+int	main(void)
 {
 	rush(132, 42);
 	return (0);
